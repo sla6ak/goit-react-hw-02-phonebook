03b41ff19b0e8963a3877b92e5746467eb0df5e6
sla6ak <slabakxaker@gmail.com>
2022-03-26 16:11:30 +0200

    propsTipes

--- src/Components/NotContacts/NotContacts.jsx
+++ src/Components/NotContacts/NotContacts.jsx
@@ -1,7 +1,10 @@
 import s from './NotContacts.module.css';
+import propTypes from 'prop-types';
 
 const NotContacts = ({ text }) => {
   return <div className={s.not}>{text}</div>;
 };
-
+NotContacts.propTypes = {
+  text: propTypes.string,
+};
 export default NotContacts;
